986190242ce02ef6ae49c4dd96e1d7a5e5c26a67
chpmoreno <chpmoreno@gmail.com>
2016-10-05 21:11:17 +0200

    add the execution script

--- lectures/1/R/mle.R
+++ lectures/1/R/mle.R
@@ -1,3 +1,8 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+
 mle_estimator_lm <- function (w, phi, t) {
     if(is.vector(t) == FALSE)
       t <- as.vector(t)
@@ -6,6 +11,5 @@ mle_estimator_lm <- function (w, phi, t) {
     M <- ncol(phi) 
     phi_w <- phi %*% w[1:M]
     Sig <- par[(M + 1):(M + 1)]
-    sum(-(1 / 2) * log(2 * pi) - (1 / 2) * log(Sig^2) - (1 / (2 * Sig ^ 2)) * (t - phi_w) ^ 2)
-  }
+    sum(-(1 / 2) * log(2 * pi) - (1 / 2) * log(Sig ^ 2) - (1 / (2 * Sig ^ 2)) * (t - phi_w) ^ 2)
 }
