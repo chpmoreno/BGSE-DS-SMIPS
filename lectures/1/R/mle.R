64f21c8f7915b4e31f635eda12ae3b8e5bf1ba0f
chpmoreno <chpmoreno@gmail.com>
2016-10-05 20:46:06 +0200

    first par of the code

---
+++ lectures/1/R/mle.R
@@ -0,0 +1,11 @@
+mle_estimator_lm <- function (w, phi, t) {
+    if(is.vector(t) == FALSE)
+      t <- as.vector(t)
+    if(is.matrix(phi) == FALSE)
+      phi <- as.matrix(phi)
+    M <- ncol(phi) 
+    phi_w <- phi %*% w[1:M]
+    Sig <- par[(M + 1):(M + 1)]
+    sum(-(1 / 2) * log(2 * pi) - (1 / 2) * log(Sig^2) - (1 / (2 * Sig ^ 2)) * (t - phi_w) ^ 2)
+  }
+}
